986f26141dafeca0508fb835a7790eef393d2515
LiJinbaoGitHub <mac@Lee.local>
2016-01-13 14:28:14 +0800

    创见新工程

---
+++ Lijinbao/Lijinbao/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Lijinbao
+//
+//  Created by ma c on 16/1/13.
+//  Copyright (c) 2016年 北京尚学堂. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"WoEle"); 
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
